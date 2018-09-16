4a61276a35696427e345ccce391218e4efeb3561
MoringaSchool <dianamulungi@gmail.com>
2018-09-17 00:29:02 +0300

    prompt commented

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function finder(){
-var threesides = []
+/*var threesides = []
  threesides.push(parseInt(prompt("enter value for first side")));
 threesides.push(parseInt(prompt("enter value for second side")));
 threesides.push(parseInt(prompt("enter value for third side")));
+*/
  var side1 = threesides[0];
 var side2 = threesides[1];
 var side3 = threesides[2];
