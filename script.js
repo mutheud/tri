174b91e1f4db62577cc56de679b6c8212e1c124f
MoringaSchool <dianamulungi@gmail.com>
2018-09-17 00:40:41 +0300

    prompt deleted

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function finder(){
-/*var threesides = []
- threesides.push(parseInt(prompt("enter value for first side")));
-threesides.push(parseInt(prompt("enter value for second side")));
-threesides.push(parseInt(prompt("enter value for third side")));
-*/
+var threesides = []
+ threesides.push(parseInt());
+threesides.push(parseInt());
+threesides.push(parseInt());
+
  var side1 = threesides[0];
 var side2 = threesides[1];
 var side3 = threesides[2];
