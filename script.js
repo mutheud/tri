ab565af058563d0d802a69f5083246cc59d5fa6a
MoringaSchool <dianamulungi@gmail.com>
2018-09-17 00:21:53 +0300

    if condition added

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ threesides.push(parseInt(prompt("enter value for third side")));
  var side1 = threesides[0];
 var side2 = threesides[1];
 var side3 = threesides[2];
+
+if (side1===side2  &&  side2===side3 && side1===side3){
+output.innerHTML=(" It is an Equilateral Triangle")
+}
+ else if (side1===side2  &&  side1!==side3 || side1===side3  && side1!==side2 || side2===side3  &&  side2!==side1){
+output.innerHTML=("It is an Isosceles Triangle")
+}
+ else if(side1!==side2 && side2!==side3 && side1!==side3){
+output.innerHTML=("It is a Scalene Triangle")
+}
+else {
+alert("NOT a triangle")
+}
+}
