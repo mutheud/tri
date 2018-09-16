871f131f9742b47cd2651138316977ee473be796
MoringaSchool <dianamulungi@gmail.com>
2018-09-17 01:07:27 +0300

    output added

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ threesides.push(parseInt());
 var side2 = threesides[1];
 var side3 = threesides[2];
 
-if (side1===side2  &&  side2===side3 && side1===side3){
-alert(" It is an Equilateral Triangle")
+if(side1===side2  &&  side2===side3 && side1===side3){
+output.innerHTML=("It is an Equilateral Triangle")
 }
- else if (side1===side2  &&  side1!==side3 || side1===side3  && side1!==side2 || side2===side3  &&  side2!==side1){
-alert("It is an Isosceles Triangle")
+else if(side1===side2  &&  side1!==side3 || side1===side3  && side1!==side2 || side2===side3  &&  side2!==side1){
+output.innerHTML=("It is an Isosceles Triangle")
 }
- else if(side1!==side2 && side2!==side3 && side1!==side3){
-alert("It is a Scalene Triangle")
+else if(side1!==side2 && side2!==side3 && side1!==side3){
+output.innerHTML=("It is a Scalene Triangle")
 }
 else {
-alert("NOT a triangle")
+alert("NOT a triangle");
 }
 }
