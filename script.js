f11c52cfde1f1df327ebd5b713b9528652fc37b8
MoringaSchool <dianamulungi@gmail.com>
2018-09-17 00:20:18 +0300

    variables added and few html cahnges

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+var threesides = []
+ threesides.push(parseInt(prompt("enter value for first side")));
+threesides.push(parseInt(prompt("enter value for second side")));
+threesides.push(parseInt(prompt("enter value for third side")));
+ var side1 = threesides[0];
+var side2 = threesides[1];
+var side3 = threesides[2];
